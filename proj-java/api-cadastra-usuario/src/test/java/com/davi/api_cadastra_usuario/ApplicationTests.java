c98d416e4d2b7e3d62a620fea6d863ab720139b8
davilimas92 <davilimas92@gmail.com>
2025-04-26 18:06:24 -0300

    initial commit

---
+++ proj-java/api-cadastra-usuario/src/test/java/com/davi/api_cadastra_usuario/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.davi.api_cadastra_usuario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
